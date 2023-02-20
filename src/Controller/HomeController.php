b4de1f83e33d72d21262f25cb9314f768e6636c2
Sam <samuel.brb19@gmail.com>
2023-02-20 21:50:20 +0100

    I can render the homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,10 +7,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Models\DB;
 
-class HomeController{
+class HomeController extends AbstractController {
 
 	#[Route('/', name:'homepage')]
 	public function index () {
-		
+		return $this->render("home/home.html.twig", ["name" => "test"]);
 	}
 }
