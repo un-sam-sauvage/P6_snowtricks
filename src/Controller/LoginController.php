c79b9c9b6518446624123ca7a2641a8e0f71e412
Sam <samuel.brb19@gmail.com>
2023-02-27 13:50:12 +0100

    creating registration route

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -6,6 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class LoginController extends AbstractController {
 	//TODO: demander si je dois utiliser les users de symfony ou mes propres users ?
 	public function validateLogin (string $usernameOrEmail, string $password) {
-
+		
 	}
 }
