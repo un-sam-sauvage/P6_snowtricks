f8934673c05ac19e6e835d00003c7a2bfb819846
Sam <samuel.brb19@gmail.com>
2023-03-04 11:59:46 +0100

    login and register working navbar started

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,12 +4,16 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class HomeController extends AbstractController {
 
 	#[Route('/', name:'homepage')]
-	public function index () {
+	public function index (AuthenticationUtils $authenticationUtils) {
 		//Pour utiliser le $_SESSION il faudra passer par le $request de symfony cf: doc
-		return $this->render("home/home.html.twig");
+		$currentUsername = $authenticationUtils->getLastUsername();
+		return $this->render("home/home.html.twig", [
+			"username" => $currentUsername
+		]);
 	}
 }
